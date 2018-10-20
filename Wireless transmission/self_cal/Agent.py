884b10f5d59df25cb1ee0d0287c4eb977d340e60
wf <1475576386@qq.com>
2018-10-20 20:13:21 +0800

    增加了好多东西，先上传保存一下吧

--- Wireless transmission/self_cal/Agent.py
+++ Wireless transmission/self_cal/Agent.py
@@ -1,25 +1,38 @@
 
 
-
-
-
 class Agent(object):
-    def __init__(self, RL_brain=None, PID_brain=None, env=None):
-        self.RL_brain = RL_brain
-        self.PID_brain = PID_brain
+    def __init__(self, rl_brain=None, pid_brain=None, env=None):
+        self.RL_brain = rl_brain
+        self.PID_brain = pid_brain
         self.env = env
 
         # staic message
         self.observation = env.reset()
 
+        # Evaluation
+        self.Switch_Threshold = -1000
+        self.MAX_SIZE = 500
+        self.reward_list = []
 
     def run(self):
         # choose RL or PID?
-        pass
-        Controller = self.PID_brain
+        controller = self._choose_brain()
         # get action
-        action = Controller.choose_action(self.observation)
+        action = controller.choose_action(self.observation)
         # step
-        observation_, reward, done = self.env.step(action)
+        self.observation, reward, done = self.env.step(action)
         # collect information
+        self._collect_information(reward)
         pass
+
+    def _collect_information(self, reward):
+        if len(self.reward_list) >= self.MAX_SIZE:
+            self.reward_list.pop(0)
+        self.reward_list.append(reward)
+
+    def _choose_brain(self):
+        sum_reward = sum(self.reward_list)
+        if sum_reward > self.Switch_Threshold:
+            return self.RL_brain
+        else:
+            return self.PID_brain
